056259083c406ff3bb9e3ee806cfa047a61ae4e5
olaguru <dapoodedeyi03@gmail.com>
2024-08-06 17:44:27 +0000

    test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,24 +86,3 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
-
-    def test_get_db(self):
-        """ test method for getting the instance of db storage"""
-        dic = {"name": "cryptotechcoder"}
-        instance = State(**dic)
-        storage.new(instance)
-        storage.save()
-        get_instance = storage.get(State, instance.id)
-        self.assertEqual(get_instance, instance)
-
-    def test_count(self):
-        """ Test count method db storage"""
-        dic = {"name": "mryemi"}
-        state = State(**dic)
-        storage.new(state)
-        dic = {"name": "Mexico", "state_id": state.id}
-        city = City(**dic)
-        storage.new(city)
-        storage.save()
-        c = storage.count()
-        self.assertEqual(len(storage.all()), c)
